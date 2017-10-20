805d99a95aa3dbbede5fc13fb064bb5a215a3d7e
Jason L. Walker <jasonlanewalker@gmail.com>
2017-10-20 17:22:31 -0500

    Add project files.

---
+++ CSI.IntercomAlarm/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace CSI.IntercomAlarm
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
