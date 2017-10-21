bc0b147b74f66ba93e033a17f60fe48f51302c9a
Jason L. Walker <jasonlanewalker@gmail.com>
2017-10-21 16:18:26 -0500

    added class for store an alarm set

---
+++ CSI.IntercomAlarm/Domain/AlarmFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace CSI.IntercomAlarm.Domain
+{
+    [Serializable]
+    public class AlarmFile
+    {
+        public ObservableCollection<Alarm> Alarms
+        {
+            get
+            {
+                return Alarms;
+            }
+
+            private set
+            {
+                Alarms = value;
+            }
+        }
+
+        public AlarmFile()
+        {
+            Alarms = new ObservableCollection<Alarm>();
+        }
+
+        public void AddAlarm(Alarm alarm)
+        {
+            Alarms.Add(alarm);
+        }
+
+        public void RemoveAlarm(Alarm alarm)
+        {
+            Alarms.Remove(alarm);
+        }
+    }
+}
