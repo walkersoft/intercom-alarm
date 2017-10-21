00ba7ad155d35bc13229cd4f2ac991c9bbb28370
Jason L. Walker <jasonlanewalker@gmail.com>
2017-10-21 15:24:21 -0500

    created skeleton for Alarm class

---
+++ CSI.IntercomAlarm/Domain/Alarm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSI.IntercomAlarm.Domain
+{
+    [Serializable]
+    public class Alarm
+    {
+        public string Title { get; private set; }
+        public DateTime Time { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public Alarm(string title, DateTime time, bool isActive = true)
+        {
+            Title = title;
+            Time = time;
+            IsActive = isActive;
+        }
+    }
+}
