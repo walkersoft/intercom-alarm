afc346c9cad72838d39b6fd9dae1c1b94a6a63e1
Jason L. Walker <jasonlanewalker@gmail.com>
2017-10-21 16:09:16 -0500

    added control disabling in MainWindow for program startup in code behind

--- CSI.IntercomAlarm/MainWindow.xaml.cs
+++ CSI.IntercomAlarm/MainWindow.xaml.cs
@@ -14,14 +14,73 @@ using System.Windows.Shapes;
 
 namespace CSI.IntercomAlarm
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DoStartupControlEnablementSetup();
+        }
+
+        void DoStartupControlEnablementSetup()
+        {
+            DisableAlarmTimeSetupControls();
+            DisableAlarmGridControls();
+            DisableAlarmSettingsControls();
+        }
+
+        void DisableAlarmTimeSetupControls()
+        {
+            alarmTitleTextbox.IsEnabled = false;
+            alarmHourSelect.IsEnabled = false;
+            alarmMinuteSelect.IsEnabled = false;
+            alarmAmPmSelect.IsEnabled = false;
+            applyAlarmButton.IsEnabled = false;
+        }
+
+        void EnableAlarmTimeSetupControls()
+        {
+            alarmTitleTextbox.IsEnabled = true;
+            alarmHourSelect.IsEnabled = true;
+            alarmMinuteSelect.IsEnabled = true;
+            alarmAmPmSelect.IsEnabled = true;
+            applyAlarmButton.IsEnabled = true;
+        }
+
+        void DisableAlarmGridControls()
+        {
+            currentAlarmsGrid.IsEnabled = false;
+            removeAlarmButton.IsEnabled = false;
+            saveAlarmsButton.IsEnabled = false;
+        }
+
+        void EnableAlarmGridControls()
+        {
+            currentAlarmsGrid.IsEnabled = true;
+            removeAlarmButton.IsEnabled = true;
+            saveAlarmsButton.IsEnabled = true;
+        }
+
+        void DisableAlarmSettingsControls()
+        {
+            alarmSoundFilenameTextbox.IsEnabled = false;
+            browseForSoundButton.IsEnabled = false;
+        }
+
+        void EnableAlarmSettingControls()
+        {
+            alarmSoundFilenameTextbox.IsEnabled = true;
+            browseForSoundButton.IsEnabled = true;
+        }
+
+        void DefaultAlarmCheckChanged(object sender, RoutedEventArgs e)
+        {
+            DisableAlarmSettingsControls();
+
+            if ((bool)useDefaultSoundCheckbox.IsChecked == false)
+            {
+                EnableAlarmSettingControls();
+            }
         }
     }
 }
